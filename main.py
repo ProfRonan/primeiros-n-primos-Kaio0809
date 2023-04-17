e51bc80a70991ce6efcaf7069f83084a8fd0c682
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-04-17 14:06:41 +0000

    exercicio concluído

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 n = int(input("Digite um número: "))
-if n >= 1:
-    print("2")
-    primo = 1
-    prox_primo = 3
-    while primo < n:
-        x = 3
-        while(x < prox_primo):
-            if prox_primo % x == 0:
-                break
-            x = x + 2
-        if x == prox_primo:
-            print(x)
-            primo = primo + 1
-        prox_primo = prox_primo + 2
+print("2")
+primo = 1
+prox_primo = 3
+while primo < n:
+    x = 3
+    while(x < prox_primo):
+        if prox_primo % x == 0:
+            break
+        x = x + 2
+    if x == prox_primo:
+        print(x)
+        primo = primo + 1
+    prox_primo = prox_primo + 2
